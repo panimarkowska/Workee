2788213d3afaf32b03a2178b80d7d751d1b23389
Michal Kamiński <michall.kaminskii@gmail.com>
2017-04-25 23:52:07 +0200

    debugger będziemy wykorzystywali do sprawnego wyszukiwania błędów w aplikacji poprzez wyświetlanie logów w konsoli

---
+++ www/js/debugger.js
@@ -0,0 +1,46 @@
+if (typeof app == 'undefined') {
+	app = {};
+}
+app.debugMode = (function() {
+	var isDebug = true;
+	var isMock = false;
+
+	var enableDebug = function (){
+		isDebug = true
+	}
+
+	var disableDebug = function (){
+		isDebug = false
+	}
+
+	var getMock = function (name){
+		if (dataMock[name]) {
+			log('Data mock name: ' + name, dataMock[name]);
+			return dataMock[name];
+		} else {
+			return null;
+		}
+	}
+
+	var log = function (message, obj){
+		if (isDebug) {
+			console.log(message + ': ', obj);
+		}
+	}
+
+	var error = function (message, obj){
+		if (isDebug) {
+			console.error(message + ': ', obj);
+		}
+	}
+
+	return {
+		isDebug : isDebug,
+		isMock : isMock,
+		enableDebug : enableDebug,
+		disableDebug : disableDebug,
+		getMock : getMock,
+		log : log,
+		error : error
+	}
+})()
