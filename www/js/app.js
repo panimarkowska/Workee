0a07d76ed8c7ec7e6d926fd0e9a98d334bb5c662
panimarkowska <panimarkowska@gmail.com>
2017-04-26 21:30:37 +0200

    LoginPage - logowanie na podstawie ciasteczka

--- www/js/app.js
+++ www/js/app.js
@@ -4,10 +4,20 @@ if (typeof app == 'undefined') {
 function init() {
     workee = workeeApi()
 	var onDeviceReady = function onDeviceReady() {
+	    var cookies = document.cookie.split("; ");
+	    for (var i=0; i<cookies.length; i++){
+            if (cookies[i].indexOf("userLogged") != -1){
+                var userLogged = cookies[i].split("=")[1];
+                if (userLogged=="1"){
+                    location.hash = "#newsPage";
+                }
+            }
+	    }
+
+
 		app.showUserDetail(102);
 	}
 	document.addEventListener("deviceready",onDeviceReady, false);
-
 }
 
 app.showUserDetail = function (id) {
@@ -19,16 +29,20 @@ app.login = function (){
     var loginValue = document.getElementById("loginValue").value;
     var passwordValue = document.getElementById("passwordValue").value;
 
-    if (loginValue.length>0 && passwordValue.length>0){
-    var data = workee.login(loginValue, passwordValue);
 
-    if (data.isLogged){
-        location.hash = "newsPage";
-    } else {
-        alert("Bledny login lub haslo!");
+    if (loginValue.length>0){
+        if (passwordValue.length>0){
+            var data = workee.login(loginValue, passwordValue);
+            if (data.isLogged){
+                document.cookie = "userLogged=1";
+                location.hash = "#newsPage";
+             }
+        }
+        else{
+            alert("Invalid password.")
+        }
     }
-
-    } else{
-    alert("Fill in!")
+    else{
+        alert("Invalid login.")
     }
 }
