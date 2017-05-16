dfb133659b91e68035185e2f2b221b74c8b7e01a
Michal Kamiński <michall.kaminskii@gmail.com>
2017-05-16 19:41:04 +0200

    dialogPage: okno błędu, ostrzeżenia, informacyjne app.formsValidation: metoda walidująca wszystkie formularze usersPage: wyszukiwarka użytkowników

--- www/js/app.js
+++ www/js/app.js
@@ -4,42 +4,61 @@ if (typeof app == 'undefined') {
 function init() {
     app.setHeaderAndFooter();
     workee = workeeApi();
+    app.formsValidation();
 
-	var onDeviceReady = function onDeviceReady() {
-	    // app.menu();
-	    var cookies = document.cookie.split("; ");
-	    for (var i=0; i<cookies.length; i++){
-            if (cookies[i].indexOf("userLogged") != -1){
-                var userLogged = cookies[i].split("=")[1];
-                if (userLogged=="1"){
-                    $.mobile.changePage($("#newsPage"), { transition: 'slidedown' });
-                } else {
-                    $.mobile.changePage($("#loginPage"), { transition: 'slidedown' });
-                }
-            } else {
-                 $.mobile.changePage($("#loginPage"), { transition: 'slidedown' });
+	var onDeviceReady = function () {
+	    app.menu();
+
+        if (app.isLogin()){
+            $.mobile.changePage($("#newsPage"));
+        } else {
+            $.mobile.changePage($("#loginPage"));
+        }
+	    $(document).on("pagebeforeshow", function(event) {
+            if (!app.isLogin()){
+                $.mobile.changePage($("#loginPage"));
             }
-	    }
-	    // app.hideLoadingPage();
-        // $(document).on( "hashchange", function() {
-        //     app.showLoadingPage();
-        // });
-        // $(document).on( "pageshow", function() {
-	     //    app.hideLoadingPage();
-        // });
+        });
+        $(document).on("pagechange", function(event) {
+            var activePage = $.mobile.pageContainer.pagecontainer("getActivePage");
+            var actionMethod = activePage.attr('data-action-method');
+            if (actionMethod && app[actionMethod]) {
+                app[actionMethod]();
+            }
+        });
 	}
 	document.addEventListener("deviceready",onDeviceReady, false);
 }
 
-app.menu = function() {
-    var menuIsActive = false;
-    var switchMenu = function(){
-        if (menuIsActive) {
-            $.mobile.back();
-            menuIsActive = false;
-        } else {
-            menuIsActive = true;
+app.formsValidation = function() {
+    $('.formValidation').validate({
+        submitHandler: function(formEl) {
+            var form = $(formEl)
+            var methodName = form.attr('data-method');
+            if (methodName && app[methodName]) {
+                params = {};
+                form.find('input').each(function() {
+                    var key = $(this).attr("name")
+                    if (key && key != 'submit') {
+                        params[key] = $(this).val();
+                    }
+                });
+                app[methodName](params);
+            }
+        },
+        invalidHandler: function(event, validator) {
+            console.log('błędny formularz', validator)
+        }
+    });
+}
 
+app.menu = function() {
+    var switchMenu = function(swipe){
+        var activePage = $.mobile.pageContainer.pagecontainer("getActivePage");
+        if (activePage.attr('id') == "menuPage" && swipe != "right") {
+            $.mobile.back();
+        } else {
+            $.mobile.changePage($("#menuPage"));
         }
     };
 
@@ -47,13 +66,10 @@ app.menu = function() {
         switchMenu();
     });
     $(document).on( "swiperight", function() {
-        switchMenu();
+        switchMenu("right");
     });
-    $(document).on( "pagechange", function() {
-            if (location.hash != '#menuPage') {
-                $('.menu').find('.ui-btn-active').removeClass('ui-btn-active');
-                $('.menu').find('a[href="' + location.hash + '"]').addClass('ui-btn-active');
-            }
+    $(document).on( "swipeleft", function() {
+        $.mobile.back();
     });
 }
 
@@ -63,16 +79,10 @@ app.setHeader = function (title) {
         var header = $('<div data-role="header" data-position="fixed">');
         page.prepend(header);
 
-        // header.load("../header.html", function () {
-        //     var headerTitle = title || $(this).parent().attr('data-header-title');
-        //     if (headerTitle) {
-        //         $(this).find('.headerTitle').text(headerTitle);
-        //     }
-        // });
         var headerGrid = '<div class="ui-grid-b ui-responsive">' +
 	                        '<div class="ui-grid-b">' +
                                 '<div class="ui-block-a">' +
-                                    '<a href="#menuPage" class="ui-icon-bars ui-btn-icon-left menuButton"></a>' +
+                                (page.attr('data-header-menu') == "false" ? '' : '<a href="#menuPage" class="menuButton ui-icon-bars ui-btn-icon-left"</a>')  +
                                 '</div>' +
                                 '<div class="ui-block-b">' +
                                     '<p class="headerTitle"></p>' +
@@ -82,9 +92,9 @@ app.setHeader = function (title) {
                         '</div>';
         header.append(headerGrid).trigger("create");
         var headerTitle = title || page.attr('data-header-title');
-            if (headerTitle) {
-                header.find('.headerTitle').text(headerTitle);
-            }
+        if (headerTitle) {
+            header.find('.headerTitle').text(headerTitle);
+        }
     } else {
         page.find('[data-role="header"] p').text(title);
     }
@@ -113,12 +123,28 @@ app.setHeaderAndFooter = function () {
     });
 }
 
+app.isLogin = function (){
+    var cookies = document.cookie.split("; ");
+    for (var i=0; i<cookies.length; i++){
+        if (cookies[i].indexOf("userLogged") != -1){
+            var userLogged = cookies[i].split("=")[1];
+            if (userLogged=="1"){
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+             return false;
+        }
+    }
+    return workee.isLogin();
+}
+
 app.login = function (){
     var loginValue = document.getElementById("loginValue").value;
     var passwordValue = document.getElementById("passwordValue").value;
 
-
-    if (loginValue.length>0){
+    if (loginValue.length > 0){
         if (passwordValue.length>0){
             var data = workee.login(loginValue, passwordValue);
             if (data.isLogged){
@@ -137,13 +163,38 @@ app.login = function (){
 
 app.logout = function (){
     document.cookie = "userLogged=0";
-    $.mobile.changePage($('#loginPage'), {
-        allowSamePageTransition: true,
-        transition: 'none',
-        reloadPage: false
-    });
+    $.mobile.changePage($('#loginPage'));
 
 }
+
+app.getUsers = function (){
+    var users = workee.getUsers();
+    var htmlList = ''
+    for(var i=0; i < users.length; i++){
+        var user = users[i];
+        htmlList += '<li><a href="#" onclick="app.getUser(' + user.id + ')">' + user.name + ' ' + user.surname + '</a></li>'
+    }
+    $('#getPeopleResult').append(htmlList).listview( "refresh" );
+}
+
+app.getUser = function (id){
+    var user = workee.getUser(id);
+    $('#employeeData').append(user.email + ' '+ user.position);
+    $.mobile.changePage($('#userPage'));
+    app.setHeader(user.name + ' '+ user.surname)
+
+}
+
+
+
+
+
+
+
+
+
+
+
 
 app.showLoadingPage = function (msg){
     var loadingEl = $('#loadingPage');
@@ -157,4 +208,41 @@ app.showLoadingPage = function (msg){
 
 app.hideLoadingPage = function (){
      $('#loadingPage').hide();
+}
+
+app.showDialogPage = function (status, headerTitle, bodyTitle, message, time){
+        var dialogPage = $('#dialogPage');
+        dialogPage.removeClass('success');
+        dialogPage.removeClass('warning');
+        dialogPage.removeClass('error');
+        dialogPage.addClass(status);
+
+        $('#dialogHeaderTitle').text(headerTitle);
+        $('#dialogTitle').text(bodyTitle);
+        $('#dialogTitle').text(bodyTitle);
+        $('#dialogMessage').text(message);
+
+
+        $.mobile.changePage($("#dialogPage"), {});
+        if (time) {
+            $('#dialogTimer').text(time + " sec");
+            $('#dialogTimer').removeClass();
+            $('#dialogTimer').addClass(status);
+            timer = 1
+            var timerToClose = setInterval(function(){
+                $('#dialogTimer').text(time-timer + " sec");
+                timer++;
+            }, 1000);
+
+            setTimeout(function(){
+                clearInterval(timerToClose);
+                app.hideDialogPage();
+            }, time * 1000);
+        } else {
+            $('#dialogTimer').text('');
+        }
+}
+
+app.hideDialogPage = function (){
+    $.mobile.back();
 }
